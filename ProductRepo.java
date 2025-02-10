b8b23d43bcfa804630ff8b5a18fc39807bffde86
Virat0601 <virat.inbox0601@gmail.com>
2025-02-10 14:58:28 +0530

    Product Module Uploaded

---
+++ ProductRepo.java
@@ -0,0 +1,11 @@
+package com.codekul.codekulECommerece.repository;
+
+import com.codekul.codekulECommerece.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+}
